48f320e675908fc2c029ae1d1e9770a452560f27
Tan Nguyen <crazytan94@gmail.com>
2018-06-18 00:03:29 +0700

    Handle binding with state

--- vd1/src/components/content/Content.js
+++ vd1/src/components/content/Content.js
@@ -3,18 +3,55 @@ import React, { Component } from 'react';
 class Content extends Component {
     constructor(props) {
         super(props);
+        
+        this.state = {
+            status: true
+        };
     }
     
-    onEdit = () => {alert('button Edit was clicked')};
+    onEdit = () => {
+        // alert('button Edit was clicked')
+        this.setState({status: false});
+    };
 
     onRemove = () => {
         // alert("button remove was clicked");
         this.notifyWithOption("message");
     }
 
+    onSave = () => {
+        this.setState({status: true})
+    }
     notifyWithOption = (message) =>{
         alert("Button Remove was clicked: "+message);
     }
+
+    displayCheck = () => {
+        if(this.state.status === true){
+            return this.renderButton();
+        }else{
+            return this.renderForm();
+        }
+    }
+    renderButton = () => (
+        <div className='row'>
+            <div className='btn btn-group'>
+                <div className='btn btn-info' onClick={() => this.onEdit()}>Edit</div>
+                {/* <div className='btn btn-warning' onClick={this.onRemove}>Remove</div> */}
+                {/* <div className='btn btn-warning' onClick={() => this.onRemove()}>Remove</div> */}
+                {/* <div className='btn btn-warning' onClick={() => this.notifyWithOption("this is message with arrow function")}>Remove</div> */}
+                <div className='btn btn-warning' onClick={this.notifyWithOption.bind(this,"this is message with bind")}>Remove</div>
+            </div>
+        </div>
+        )
+
+    renderForm = () => (
+        <div className='row'>
+            <div className='form-group pl-3'>
+                <input type='text' name='title' className='form-control'/>
+                <div className='mt-2 btn btn-block btn-success' onClick={() => this.onSave()}>Save</div>
+            </div>
+        </div>)
 
     render() {
         return (
@@ -23,7 +60,7 @@ class Content extends Component {
                     <div className="row align-items-center">
                         <div className={"col-md-4 " + this.props.imagePosition}>
                             <div className="p-1">
-                                <img className="img-fluid rounded-circle" src={this.props.image} alt />
+                                <img className="img-fluid rounded-circle" src={this.props.image} alt="" />
                             </div>
                         </div>
                         <div className="col-md-8">
@@ -39,16 +76,7 @@ class Content extends Component {
                         <p>{this.props.content}</p>
                     </div>
                      <hr/>
-                    <div className='row'>
-                        <div className='btn btn-group'>
-                            <div className='btn btn-info' onClick={this.onEdit}>Edit</div>
-                            {/* <div className='btn btn-warning' onClick={this.onRemove}>Remove</div> */}
-                            <div className='btn btn-warning' onClick={() => this.onRemove()}>Remove</div>
-                            <div className='btn btn-warning' onClick={() => this.notifyWithOption("this is message with arrow function")}>Remove</div>
-                            <div className='btn btn-warning' onClick={this.notifyWithOption.bind(this,"this is message with bind")}>Remove</div>
-                        </div>
-                    </div>
-                   
+                    {this.displayCheck()}
                 </div>
           
         );
